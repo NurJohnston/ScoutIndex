3635be8e0ef2dbbead0f2eaeb90dd5c40ab1ca2b
Nur3000 <127439973+NurJohnston@users.noreply.github.com>
2026-06-30 14:37:59 +0200

    Update App.py

--- App.py
+++ App.py
@@ -20,7 +20,9 @@ except FileNotFoundError:
     pass
 
 if not API_KEY:
-    API_KEY = input("Paste your Gemini API key: ")
+    #API_KEY = input("Paste your Gemini API key: ")
+    import os
+    API_KEY = os.environ.get("GEMINI_API_KEY", "")
 
 url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key={API_KEY}"
 
